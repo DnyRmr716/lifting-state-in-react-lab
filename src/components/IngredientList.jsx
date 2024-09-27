d383a2babc9264d5d3dc57ca7d78b9990544327b
DannyR716 <01dromero10@gmail.com>
2024-09-26 23:42:43 -0400

    Finished Lab

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const IngredientList = ({ availableIngredients, addToBurger }) => {
+  return (
+    <ul>
+      {availableIngredients.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button onClick={() => addToBurger(ingredient)}>+</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default IngredientList;
